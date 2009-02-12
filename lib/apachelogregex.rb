794b4238eff8169af93387010f77564dbe2393e6
Simone Carletti <weppos@weppos.net>
2009-02-12 22:54:00 +0100

    Added apachelogregex file to simplify GEM usage. Now you can simply require the library with
    require 'rubygems'
    require 'apachelogregex'
    
    instead of
    
    require 'rubygems
    gem      'apachelogregex'
    require 'apache_log_regex'

---
+++ lib/apachelogregex.rb
@@ -0,0 +1,16 @@
+#
+# = Apache Log Regex
+#
+# Ruby parser for Apache log files based on regular expressions.
+#
+# Category::    
+# Package::     ApacheLogRegex
+# Author::      Simone Carletti <weppos@weppos.net>
+# License::     MIT License
+#
+#--
+# SVN: $Id$
+#++
+
+
+require 'apache_log_regex'
